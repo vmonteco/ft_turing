aad00c40c14c3cf71726196f2311659cf99c9065
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2025-12-12 03:01:26 +0100

    added tests for parse-args

--- src/tests/tests.lisp
+++ src/tests/tests.lisp
@@ -2,5 +2,36 @@
 
 (def-suite ft_turing-tests)
 (in-suite ft_turing-tests)
-
-;; Here would stand tests for ft_turing-pkg
+ 
+(test test-parse-args
+	  ;; Basic case:
+	  (multiple-value-bind (file input)
+		  (ft_turing-pkg::parse-args '("foo" "bar"))
+		(is (equal file "foo"))
+		(is (equal input "bar")))
+	  ;; -h/--help cases:
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("-h")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("--help")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("-h" "foo")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("--help" "foo")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("-h" "foo" "bar")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("--help" "foo" "bar")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("foo" "-h" "bar")))
+	  (signals ft_turing-pkg::help-condition
+			   (ft_turing-pkg::parse-args '("foo" "--help" "bar")))
+	  ;; Usage error cases:
+	  (signals ft_turing-pkg::usage-error
+			   (ft_turing-pkg::parse-args nil))
+	  (signals ft_turing-pkg::usage-error
+			   (ft_turing-pkg::parse-args '("foo")))
+	  (signals ft_turing-pkg::usage-error
+			   (ft_turing-pkg::parse-args '("-help")))
+	  (signals ft_turing-pkg::usage-error
+			   (ft_turing-pkg::parse-args '("foo" "bar" "xyz"))))
