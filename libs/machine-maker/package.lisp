3f806126377100a6b844a69629689848400bb406
Vincent MONTECOT <vmonteco@student.42.fr>
2026-01-13 19:43:01 +0100

    machine-maker started

--- libs/machine-maker/package.lisp
+++ libs/machine-maker/package.lisp
@@ -1,2 +1,12 @@
 (defpackage :machine-maker
-  (:use :cl))
+  (:nicknames "mm")
+  (:documentation "This package mostly provides a higher order function to \
+\"generate\" a Turing machine function from a machine description.")
+  (:use :cl)
+  (:depends-on :machine-description)
+  (:import-from
+   :machine-description
+   :machine-description)
+  (:export
+   :make-machine))
+  
