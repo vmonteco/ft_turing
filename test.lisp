def7c2b990cc323821754270707c734c77387b45
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2026-02-10 10:31:52 +0100

    Made quickloading silent

--- test.lisp
+++ test.lisp
@@ -9,7 +9,7 @@
 
 ;; It's possible to use asdf:load-system to load a system, but asdf
 ;; can't install dependencies (which ql:quickload can).
-(ql:quickload :ft_turing/tests :verbose t)
+(ql:quickload :ft_turing/tests :silent t)
 
 ;; This is the actual tests entrypoint.
 (asdf:test-system :ft_turing :verbose t)
